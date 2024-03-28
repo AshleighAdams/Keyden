fa18ea12540ec49f65e774886037ebee430b11a4
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2024-03-28 18:50:27 +0000

    Added design mockup for key options

--- src/KeyWarden/ViewModels/KeyOptionsViewModel.cs
+++ src/KeyWarden/ViewModels/KeyOptionsViewModel.cs
@@ -5,6 +5,30 @@ namespace KeyWarden.ViewModels;
 
 public class KeyOptionsViewModel : ViewModelBase
 {
+	public string[] KeepUnlockedDurations { get; } =
+	[
+		"1 minute",
+		"2 minutes",
+		"5 minutes",
+		"10 minutes",
+		"30 minutes",
+		"1 hour",
+		"4 hours",
+		"8 hours",
+		"ever",
+	];
+	public string[] InactivityDurations { get; } =
+	[
+		"1 minute",
+		"2 minutes",
+		"5 minutes",
+		"10 minutes",
+		"30 minutes",
+		"1 hour",
+		"4 hours",
+		"8 hours",
+	];
+
 	private ObservableSshKey? _Key;
 	public ObservableSshKey? Key
 	{
