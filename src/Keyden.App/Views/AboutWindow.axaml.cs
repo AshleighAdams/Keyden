0ef9323decf0b39341b4cc25eff69cde8bfaa9bf
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2024-09-14 07:40:51 +0100

    Added licenses to the about page

--- src/Keyden.App/Views/AboutWindow.axaml.cs
+++ src/Keyden.App/Views/AboutWindow.axaml.cs
@@ -15,5 +15,27 @@ namespace Keyden.Views
 				Acrylic.Material.MaterialOpacity = 0;
 			}
 		}
+
+		public string MitLicense =>
+			"""
+			Permission is hereby granted, free of charge, to any person obtaining a copy
+			of this software and associated documentation files (the "Software"), to deal
+			in the Software without restriction, including without limitation the rights
+			to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+			copies of the Software, and to permit persons to whom the Software is
+			furnished to do so, subject to the following conditions:
+			
+			The above copyright notice and this permission notice shall be included in all
+			copies or substantial portions of the Software.
+			""";
+
+		public string Apache2License =>
+			"""
+			Licensed under the Apache License, Version 2.0 (the "License");
+			you may not use this file except in compliance with the License.
+			You may obtain a copy of the License at
+
+			http://www.apache.org/licenses/LICENSE-2.0
+			""";
 	}
 }
