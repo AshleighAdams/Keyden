9ab935eb3399999aa21e471dc7c2ac1b76b62c24
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2025-10-01 10:03:53 +0100

    Only allow a single instance at a time

--- src/Keyden.App/App.axaml.cs
+++ src/Keyden.App/App.axaml.cs
@@ -7,11 +7,16 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-using Keyden.ViewModels;
 using Keyden.Views;
 using Avalonia.Controls;
 using System.Text.Json;
 using System.Text;
+using System.IO.Pipes;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Reflection.Metadata;
+using System.Threading;
 
 namespace Keyden;
 
@@ -55,8 +60,100 @@ public partial class App : Application
 	private SshAgent? Agent { get; set; }
 	private KeydenSettings? Settings { get; set; }
 
+	public static string AppPipePath
+	{
+		get
+		{
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+				return "keyden";
+
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				var uid = Unix.Getuid().ToString(CultureInfo.InvariantCulture);
+				var socketDirectory = $"/run/user/{uid}";
+
+				// check for legacy linux systems
+				if (!Directory.Exists(socketDirectory))
+					socketDirectory = Directory.Exists("/run") ? "/run" : "/var/run";
+
+				if (!Directory.Exists(socketDirectory))
+					return "keyden";
+
+				socketDirectory += "/keyden";
+				var socketPath = $"{socketDirectory}/keyden.sock";
+
+				if (!Directory.Exists(socketDirectory))
+					Directory.CreateDirectory(socketDirectory);
+				return socketPath;
+			}
+
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				var uid = Unix.Getuid().ToString(CultureInfo.InvariantCulture);
+				return $"/var/run/{uid}-keyden.sock";
+			}
+
+			return "keyden";
+		}
+	}
+
+	private async void MainAppPipeThread(CancellationToken ct)
+	{
+		using var pipeServer = new NamedPipeServerStream(
+				pipeName: AppPipePath,
+				direction: PipeDirection.InOut,
+				maxNumberOfServerInstances: NamedPipeServerStream.MaxAllowedServerInstances,
+				transmissionMode: PipeTransmissionMode.Byte,
+				options: PipeOptions.Asynchronous | PipeOptions.WriteThrough | PipeOptions.CurrentUserOnly,
+				inBufferSize: 1,
+		outBufferSize: 1);
+
+		while (!ct.IsCancellationRequested)
+		{
+			try
+			{
+				await pipeServer.WaitForConnectionAsync(ct);
+				var byteRead = pipeServer.ReadByte();
+				if (byteRead == 0x0A)
+				{
+					ShowMainWindow();
+					pipeServer.WriteByte(0xA0);
+					pipeServer.Flush();
+				}
+			}
+			catch (OperationCanceledException) { }
+			catch (IOException) { }
+			finally
+			{
+				if (pipeServer.IsConnected)
+					pipeServer.Disconnect();
+			}
+		}
+	}
+
+	private CancellationTokenSource AppPipeCts = new();
+
 	public override void OnFrameworkInitializationCompleted()
 	{
+		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime)
+		{
+			try
+			{
+				using var client = new NamedPipeClientStream(pipeName: AppPipePath);
+				if (client.IsConnected)
+				{
+					client.ReadTimeout = 1000;
+					client.WriteTimeout = 1000;
+					client.WriteByte(0x0A);
+					client.Flush();
+					if (client.ReadByte() == 0xA0)
+						Environment.Exit(0);
+				}
+			}
+			catch (IOException) { }
+			catch (TimeoutException) { }
+		}
+
 		var collection = new ServiceCollection();
 
 		collection.AddSingleton(SystemServices);
@@ -70,18 +167,25 @@ public partial class App : Application
 		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
 		{
 			desktop.ShutdownMode = ShutdownMode.OnExplicitShutdown;
-
 			bool isAutomaticStart =
 				SystemServices.IsAutomaticStart ||
 				desktop.Args?.Contains("--hide") == true;
 
 			if (!isAutomaticStart)
 				ShowMainWindow();
+
+			MainAppPipeThread(AppPipeCts.Token);
+			desktop.Exit += DesktopAppExit;
 		}
 		else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
 			singleViewPlatform.MainView = new MainView();
 
 		base.OnFrameworkInitializationCompleted();
+	}
+
+	private void DesktopAppExit(object? sender, ControlledApplicationLifetimeExitEventArgs e)
+	{
+		AppPipeCts.Cancel();
 	}
 
 	private static SettingsWindow? SettingsWindow { get; set; }
